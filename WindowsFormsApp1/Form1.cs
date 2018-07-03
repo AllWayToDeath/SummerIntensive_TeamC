75122d728b0ed6a1d6fadaebe7b80c4fbe8aeef1
Олег <ov.dragon2018@gmail.com>
2018-07-03 05:32:39 +0400

    Баг решен + бд

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -5,44 +5,55 @@ using System.Diagnostics;
 using NAudio.Wave;
 using Microsoft.Win32;
 using System.Threading;
-using System.Collections.Generic;
-using System.Collections;
-using System.IO;
-using System.Configuration;
-using SlackRecorder.Model;
-using System.Data.SqlClient;
-using System.ComponentModel;
+using System.Data.SQLite;
+using System.Data;
+using TagLib.Mpeg;
+using TagLib;
+using NAudio.Lame;
+using NAudio.CoreAudioApi;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        WaveInEvent waveIn;
+        string sql = "Select Name, Path, Duration From Records"; //Sql запрос (достать все из таблицы customer)
+
+        static string path = "DBase.db"; //Путь к файлу БД
+
+        string ConnectionString = "Data Source=" + path + ";Version=3;New=True;Compress=True;"; //Строка соеденения (так хочет sqlite)
+        SQLiteConnection conn = null;
+        IWaveIn waveIn;
         WaveFileWriter writer;
 
-        WasapiLoopbackCapture CaptureInstance;
+        IWaveIn CaptureInstance;
         WaveFileWriter RecordedAudioWriter;
-        
+
         FolderBrowserDialog saveDirectory = new FolderBrowserDialog();
-        
+
         ManagementEventWatcher startWatch;
         ManagementEventWatcher stopWatch;
-
-        Repository _rep;
-        BindingList<Record> bindingList;
+        string resultfile = "Result.wav";
+        string mp3File = (DateTime.Now.ToShortDateString() + "_" + DateTime.Now.ToShortTimeString() + ".mp3").Replace(":", "_");
 
         string playbackRecordFileName = "Record_from_speakers.wav";
         string micRecordFIleName = "Record_from_mic.wav";
 
         public Form1()
-        {
-            _rep = new Repository();
-
+        { 
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
+            conn = new SQLiteConnection(ConnectionString); //Создаем соединение
+            conn.Open();
+            DataTable ds = Load_Db(conn);
+
+            dataGridView1.DataSource = ds;
+           
+            
+
             startWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace WHERE ProcessName = \"Slack.exe\""));
             startWatch.EventArrived += new EventArrivedEventHandler(startWatch_EventArrived);
             startWatch.Start();
@@ -53,26 +64,26 @@ namespace WindowsFormsApp1
 
             saveDirectory.SelectedPath = saveDirectoryTextBox.Text;
 
-            //dataGridView1.Dispose();
 
-            FeelGrid();
+            
         }
 
-        private void ReadRecords()
-        {           
-            List<Record> clients = _rep.GetRecords();
+       
 
-            bindingList = new BindingList<Record>(clients);
-            this.dataGridView1.DataSource = new BindingSource(bindingList, null);
-        }
-
-        void FeelGrid()
-        {
-            ReadRecords();
-        }
-
+ 
         public static int counter = 0;
+        DataTable Load_Db(SQLiteConnection conn)
+        {
+            SQLiteCommand sql = new SQLiteCommand();
+            sql.CommandText ="Select Name, Path, Duration From Records";
+            sql.Connection = conn;
+            DataTable ds = new DataTable();
+            SQLiteDataReader da = sql.ExecuteReader();
 
+            ds.Load(da);//Заполняем DataSet cодержимым DataAdapter'a
+
+            return ds;//Заполняем созданный на форме dataGridView1//Создаем объект класса DataAdapter (тут мы передаем наш запрос и получаем ответ)
+        }
         void stopWatch_EventArrived(object sender, EventArrivedEventArgs e)
         {
             Process[] proc = Process.GetProcessesByName("Slack");
@@ -84,70 +95,91 @@ namespace WindowsFormsApp1
                     notifyIcon.ShowBalloonTip(1000, "Slack Recorder", "Call recorded", ToolTipIcon.Info);
                     notifyIcon.Visible = false;
 
-                    if (waveIn != null)
+                   if (waveIn == null && CaptureInstance == null)
                     {
+                        return;
+
+                    }
                         waveIn.StopRecording();
-                        this.CaptureInstance.StopRecording();
+                        CaptureInstance.StopRecording();
 
                         MixTwoSamples();
 
-                        ConvertToMP3();
+                        ConvertToMP3(saveDirectory.SelectedPath + "//" + resultfile, saveDirectory.SelectedPath + "//" + mp3File, 320);
+                        DeleteTempFile();
+                        AddToDatabase();
 
-                        AddToDatabase();                       
-                    }
+                    
+
 
                     counter = 0;
                 }
                 counter++;
             }
         }
-
+        void DeleteTempFile()
+        {
+            System.IO.File.Delete(saveDirectory.SelectedPath + "//" + micRecordFIleName);
+            System.IO.File.Delete(saveDirectory.SelectedPath + "//" + playbackRecordFileName);
+            System.IO.File.Delete(saveDirectory.SelectedPath + "//" + resultfile);
+        }
         void AddToDatabase()
         {
-            Record record = new Record();
+            string Name = "";
+            string Path = "";
+            string Duration = "";
+            using (var audio = new AudioFile(saveDirectory.SelectedPath + "\\" + mp3File, ReadStyle.Average))
+            {
+                Name = mp3File;
+                Path = saveDirectory.SelectedPath;
+                Duration = audio.Properties.Duration.ToString("hh\\:mm\\:ss");
+            };
+            Mp3File file = new Mp3File(Name, Path, Duration);
 
-            record.Date = DateTime.Now.ToString("dd.MM.yyyy");
-            record.Time = DateTime.Now.ToString("HH.mm");
 
-            _rep.Insert(record);
+            SQLiteCommand CMD = conn.CreateCommand();
+            CMD.CommandText = "insert into Records(Name, Path, Duration) values (@name, @path, @duration )";
+            CMD.Parameters.AddWithValue("@name", file.get_Name() );
+            CMD.Parameters.AddWithValue("@path", file.get_Path() );
+            CMD.Parameters.AddWithValue("@duration", file.get_Duration() );
+            CMD.ExecuteNonQuery();
 
-            //FeelGrid(); вызывает ошибку при повторном использовании
+           
         }
 
         void MixTwoSamples()
         {
-            NAudio.Wave.SampleProviders.MixingSampleProvider mixer = new NAudio.Wave.SampleProviders.MixingSampleProvider(NAudio.Wave.WaveFormat.CreateIeeeFloatWaveFormat(44100, 2));
+            if (writer == null && RecordedAudioWriter == null)
+            {
+                using (AudioFileReader _in = new AudioFileReader(saveDirectory.SelectedPath + "//" + micRecordFIleName))
+                using (AudioFileReader _out = new AudioFileReader(saveDirectory.SelectedPath + "//" + playbackRecordFileName))
+                {
+                    WaveMixerStream32 res = new WaveMixerStream32();
 
-            //Найти способ избавиться от этого мсгбокса, без него AudioFileReader не инициализируется
-            MessageBox.Show("Click me to get a record");
-            
-            AudioFileReader MicFileReader = new AudioFileReader(saveDirectory.SelectedPath + "\\" + micRecordFIleName);
-            AudioFileReader SpeakerFilereader = new AudioFileReader(saveDirectory.SelectedPath + "\\" + playbackRecordFileName);
+                    res.AddInputStream(_in);
+                    res.AddInputStream(_out);
 
-            AddSampleInMixer(mixer, MicFileReader);
-            AddSampleInMixer(mixer, SpeakerFilereader);
+                    WaveFileWriter.CreateWaveFile(saveDirectory.SelectedPath + "//" + resultfile, res);
+                }
+            }
+            else
+            {
+                writer.Dispose();
+                RecordedAudioWriter.Dispose();
+                writer = null;
+                RecordedAudioWriter = null;
+                MixTwoSamples();
 
-            var waveProvider = mixer.ToWaveProvider();
-
-            WaveFileWriter.CreateWaveFile(saveDirectory.SelectedPath + "\\" + "result.wav", waveProvider);
+            }
         }
 
-        void AddSampleInMixer(NAudio.Wave.SampleProviders.MixingSampleProvider mixer, AudioFileReader audioFileReader)
+
+
+        void ConvertToMP3(string waveFileName, string mp3FileName, int bitRate = 128)
         {
-            mixer.AddMixerInput((ISampleProvider)audioFileReader);
-        }
-
-        void ConvertToMP3()
-        {
-            ProcessStartInfo psi = new ProcessStartInfo();
-
-            psi.FileName = "lame.exe";
-            psi.Arguments = "-V2 " + saveDirectory.SelectedPath + @"\" + "result.wav " + saveDirectory.SelectedPath + @"\" + DateTime.Now.ToString("dd.MM.yyyy_HH.mm") + ".mp3";
-            psi.WindowStyle = ProcessWindowStyle.Hidden;
-
-            Process p = Process.Start(psi);
-
-            p.WaitForExit();
+            using (var reader = new AudioFileReader(waveFileName))
+            using (var writer = new LameMP3FileWriter(mp3FileName, reader.WaveFormat, bitRate))
+                reader.CopyTo(writer);
         }
 
         void startWatch_EventArrived(object sender, EventArrivedEventArgs e)
@@ -159,35 +191,51 @@ namespace WindowsFormsApp1
                 {
                     try
                     {
-                        waveIn = new WaveInEvent();
-
-                        waveIn.DeviceNumber = 0;
-                        waveIn.WaveFormat = new NAudio.Wave.WaveFormat(44100, 32, 2);
-
-                        waveIn.DataAvailable += waveIn_DataAvailable;
-                        waveIn.RecordingStopped += waveIn_RecordingStopped;
-
+                        waveIn = new WasapiCapture();
                         writer = new WaveFileWriter(saveDirectory.SelectedPath + "\\" + micRecordFIleName, waveIn.WaveFormat);
 
+
+                        waveIn.DataAvailable += (s, a) =>
+                        {
+                            writer.Write(a.Buffer, 0, a.BytesRecorded);
+                        };
+                        waveIn.RecordingStopped += (s, a) =>
+                        {
+                           
+                            if (waveIn != null)
+                            {
+                                waveIn.Dispose();
+                                waveIn = null;
+                            }
+                            if (a.Exception != null)
+                            {
+                                throw a.Exception;
+                            }
+                        };
                         waveIn.StartRecording();
 
-                        this.CaptureInstance = new WasapiLoopbackCapture();
-                        this.RecordedAudioWriter = new WaveFileWriter(saveDirectory.SelectedPath + "\\"+ playbackRecordFileName, CaptureInstance.WaveFormat);
+                        CaptureInstance = new WasapiLoopbackCapture();
+                        RecordedAudioWriter = new WaveFileWriter(saveDirectory.SelectedPath + "\\"+ playbackRecordFileName, CaptureInstance.WaveFormat);
 
-                        this.CaptureInstance.DataAvailable += (s, a) =>
+                        CaptureInstance.DataAvailable += (s, a) =>
                         {
-                            this.RecordedAudioWriter.Write(a.Buffer, 0, a.BytesRecorded);
+                            RecordedAudioWriter.Write(a.Buffer, 0, a.BytesRecorded);
                         };
 
-                        this.CaptureInstance.RecordingStopped += (s, a) =>
+                        CaptureInstance.RecordingStopped += (s, a) =>
                         {
-                            this.RecordedAudioWriter.Dispose();  //здесь может быть баг
-                            this.RecordedAudioWriter = null;
-                            CaptureInstance.Dispose();
+                            
+                            if (CaptureInstance != null)
+                            {
+                                CaptureInstance.Dispose();
+                                CaptureInstance = null;
+                            }
+                            if (a.Exception != null)
+                            {
+                                throw a.Exception;
+                            }
                         };
-
-                        this.CaptureInstance.StartRecording();
-
+                        CaptureInstance.StartRecording();
                     }
 
                     catch (Exception ex)
@@ -204,34 +252,6 @@ namespace WindowsFormsApp1
                 counter++;
             }
         }
-
-        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
-        {
-            if (this.InvokeRequired)
-            {
-                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
-            }
-            else
-            {
-                writer.Write(e.Buffer, 0, e.BytesRecorded);
-            }
-        }
-
-        private void waveIn_RecordingStopped(object sender, EventArgs e)
-        {
-            if (this.InvokeRequired)
-            {
-                this.BeginInvoke(new EventHandler(waveIn_RecordingStopped), sender, e);
-            }
-            else
-            {
-                waveIn.Dispose();
-                waveIn = null;
-                writer.Close();
-                writer = null;
-            }
-        }
-
         private void browseButton_Click(object sender, EventArgs e)
         {
             if (saveDirectory.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(saveDirectory.SelectedPath))
@@ -315,29 +335,12 @@ namespace WindowsFormsApp1
 
         private void openButton_Click(object sender, EventArgs e)
         {
-            //string argument = "/select, \"" + saveDirectory.SelectedPath + dataGridView1.CurrentRow.Cells[1].Value + "_" + dataGridView1.CurrentRow.Cells[2].Value + "\"";
-
-            //Process p = Process.Start("explorer.exe", argument);
-
-            //p.WaitForExit();
-
-            string selectedFile = saveDirectory.SelectedPath + "\\" + dataGridView1.CurrentRow.Cells[1].Value.ToString() + "_" + dataGridView1.CurrentRow.Cells[2].Value.ToString() + ".mp3";
-
-            Process.Start("explorer.exe", string.Format("/select,\"{0}\"", selectedFile));
+           
         }
 
         private void deleteButton_Click(object sender, EventArgs e)
         {
-            Record record = (Record)dataGridView1.CurrentRow.DataBoundItem;
-            if (!record.Id.HasValue)
-            {
-                MessageBox.Show("Select a row first");
-            }
-            else
-            {
-                _rep.Delete(record);
-                ReadRecords();
-            }
+            
         }
 
         private void runAtStartUpCheckBox_CheckedChanged(object sender, EventArgs e)
---
+++ WindowsFormsApp1/Mp3File.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Mp3File
+    {
+        private string Name;
+        private string Path;
+        private string Duration;
+
+        public string get_Name()
+        {
+            return Name;
+        }
+        public string get_Path()
+        {
+            return Path;
+        }
+
+        public string get_Duration()
+        {
+            return Duration;
+        }
+        public void set_Name(string name)
+        {
+            Name = name;
+        }
+        public void set_Path(string path)
+        {
+           Path = path;
+        }
+
+        public void set_Duration(string duration)
+        {
+            Duration= duration;
+        }
+
+        public Mp3File(string name, string path, string duration)
+        {
+            Name = name;
+            Path = path;
+            Duration = duration;
+
+        }
+
+
+
+    }
+}
